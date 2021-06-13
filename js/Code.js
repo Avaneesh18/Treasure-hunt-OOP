5c6ddca1cfeb6e7df3c8271e2c7d2b6e445c86bb
Avaneesh18 <78967824+Avaneesh18@users.noreply.github.com>
2021-06-13 15:29:55 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "Graphics Interchange Format";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 120,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 120,70)
+
+    fill("white")
+    textSize(15)
+    //text("H S G G R T P E R A C O A H N M N C A I R T F E I ", 515,150)
+    text("H S G G R T P E R A C O A H N M N C A I R T F E I ", 515,350)
+    fill("lightblue")
+    text("Hint:  image files that supports both animated and static images!!", 515,370)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+}
+
+
+
